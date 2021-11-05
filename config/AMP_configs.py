987e3f8138751096af5270d403d5f3480ed86371
Leo Feng <leofeng1999@gmail.com>
2021-11-05 02:17:45 -0400

    New MetaRLBO AMP configs

--- config/AMP_configs.py
+++ config/AMP_configs.py
@@ -153,6 +153,86 @@ amp_dynappo_random = {
 # ======================= Proxy Oracles: K Nearest Regressors
 
 
+
+metarlbo_amp_knr_001 = {
+	"exp_label": "MetaRLBO-AMP-KNR-001",
+	"num_proxies": 4,
+	"num_inner_updates": 1,
+	"num_initial_samples": 250,
+	"num_query_proxies": 32,
+	"num_samples_per_proxy": 75,
+	"num_query_per_iter": 250,
+	"inner_lr": 1.0,
+	"outer_lr": 0.1,
+	"num_meta_updates_per_iter": 50,
+	"entropy_reg_coeff": 0.0,
+	"proxy_oracle": {
+		"model_name": "KNR",
+		"p": 0.8, 
+	},
+	"outerloop": {
+		"oracle": "proxy",
+		"density_penalty": True,
+	},
+	"selection_criteria": { # Configs for selecting the samples
+		"method": "UCB", 
+		"config": {
+			'beta': 4.0,
+		},
+		"diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+	},
+
+	"true_oracle": {
+		"model_name": "RFC",
+	},
+	"log_interval": 1,
+	"results_log_dir": "./logs",
+	"seed": 73,
+}
+
+metarlbo_amp_knr_002 = {
+	"exp_label": "MetaRLBO-AMP-KNR-002",
+	"num_proxies": 4,
+	"num_inner_updates": 2,
+	"num_initial_samples": 250,
+	"num_query_proxies": 32,
+	"num_samples_per_proxy": 75,
+	"num_query_per_iter": 250,
+	"inner_lr": 1.0,
+	"outer_lr": 0.1,
+	"num_meta_updates_per_iter": 50,
+	"entropy_reg_coeff": 0.0,
+	"proxy_oracle": {
+		"model_name": "KNR",
+		"p": 0.8, 
+	},
+	"outerloop": {
+		"oracle": "proxy",
+		"density_penalty": True,
+	},
+	"selection_criteria": { # Configs for selecting the samples
+		"method": "UCB", 
+		"config": {
+			'beta': 4.0,
+		},
+		"diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+	},
+
+	"true_oracle": {
+		"model_name": "RFC",
+	},
+	"log_interval": 1,
+	"results_log_dir": "./logs",
+	"seed": 73,
+}
+
+
+
+
+
+# ======================= Old configs
+
+
 amp_knr_001 = { # Increased number of proxy models...
 	"exp_label": "AMP-KNR-001",
 	"num_proxies": 4, 

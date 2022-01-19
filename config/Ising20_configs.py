3f90e6419e34d13d5f1f8049a4708cfc9b06adb2
Leo Feng <leofeng1999@gmail.com>
2022-01-19 12:00:18 -0500

    Ising20_070 configs

--- config/Ising20_configs.py
+++ config/Ising20_configs.py
@@ -3098,3 +3098,52 @@ metarlbo_ising20_069 = { # Copy 068 (but reset_policy: False -> True)
     "results_log_dir": "/home/leofeng/scratch/MetaRLBO/logs",
     "seed": 73,
 }
+
+
+metarlbo_ising20_070 = { # Copy 069 (but num_meta_updates_per_iter: 80 -> 120)
+    "exp_label": "MetaRLBO-Ising20-CNN-070",
+    "task": "AltIsing20-v0",
+    "num_proxies": 4, 
+    "max_num_queries": 8000, # Maximum number of queries in experiment
+    "num_inner_updates": 1,
+    "num_query_proxies": 32,
+    "num_initial_samples": 500,
+    "num_samples_per_proxy": 64,
+    "num_query_per_iter": 500,
+    "inner_lr": 1.0,
+    "outer_lr": 0.1,
+    "num_meta_updates_per_iter": 120, 
+    "entropy_reg_coeff": 0.2,
+    "proxy_oracle": {
+        "model_name": "CNN",
+        "p": 1.0, 
+    },
+    "outerloop": {
+        "oracle": "proxy",
+        "density_penalty": True,
+    },
+    "policy": {
+        "num_steps": 81, # number of steps (per env) before updating... ensure this is at least as big as the length of the episode of the environment
+        "num_meta_steps": 61,
+    },
+    "selection_criteria": { # Configs for selecting the samples
+        "method": "UCB", 
+        "config": {
+            'beta': 1.0,
+        },
+        "diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+    },
+    "env": { # See DynaPPO paper for these configs
+        "lambda": 1.0, # Diversity hyperparameter -- higher is more penalty for more similar mols.
+        "radius": 2, 
+    },
+
+    "true_oracle": {
+        "model_name": "AltIsing_Oracle",
+    },
+    "reset_policy_per_round": True,
+    "use_baseline": False,
+    "log_interval": 1,
+    "results_log_dir": "/home/leofeng/scratch/MetaRLBO/logs",
+    "seed": 73,
+}

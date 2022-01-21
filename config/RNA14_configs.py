e7efabfd148c39c65d159abe4d3b1e032e3d3e11
Leo Feng <leofeng1999@gmail.com>
2022-01-21 03:08:40 -0500

    RNA14 configs

--- config/RNA14_configs.py
+++ config/RNA14_configs.py
@@ -1032,3 +1032,252 @@ rna14_022 = { # 013 but num_meta_updates_per_iter: 50 -> 100
     "results_log_dir": "./logs",
     "seed": 73,
 }
+
+
+rna14_023 = { # 020 but radius: 2 -> 3
+    "exp_label": "MetaRLBO-RNA14-023",
+    "task": "RNA14-v0",
+    "num_proxies": 4, 
+    "max_num_queries": 1500, # Maximum number of queries in experiment
+    "num_inner_updates": 2,
+    "num_query_proxies": 32,
+    "num_initial_samples": 100,
+    "num_samples_per_proxy": 64,
+    "num_query_per_iter": 100,
+    "inner_lr": 1.0,
+    "outer_lr": 0.1,
+    "num_meta_updates_per_iter": 50, 
+    "entropy_reg_coeff": 0.2,
+    "proxy_oracle": {
+        "model_name": "CNN",
+        "p": 1.0, 
+    },
+    "policy": {
+        "num_steps": 58, # number of steps (per env) before updating... ensure this is at least as big as the length of the episode of the environment
+        "num_meta_steps": 58,
+    },
+    "outerloop": {
+        "oracle": "proxy",
+        "density_penalty": True,
+    },
+    "selection_criteria": { # Configs for selecting the samples
+        "method": "UCB", 
+        "config": {
+            'beta': 1.0,
+        },
+        "diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+    },
+    "env": { # See DynaPPO paper for these configs
+        "lambda": 0.1, # Diversity hyperparameter -- higher is more penalty for more similar mols.
+        "radius": 2, 
+    },
+
+    "true_oracle": {
+        "model_name": "RNA14_Oracle",
+    },
+    "reset_policy_per_round": True,
+    "use_baseline": False,
+    "log_interval": 1,
+    "results_log_dir": "./logs",
+    "seed": 73,
+}
+
+
+rna14_024 = { # 020 but num_inner_updates: 2 -> 3
+    "exp_label": "MetaRLBO-RNA14-024",
+    "task": "RNA14-v0",
+    "num_proxies": 4, 
+    "max_num_queries": 1500, # Maximum number of queries in experiment
+    "num_inner_updates": 2,
+    "num_query_proxies": 32,
+    "num_initial_samples": 100,
+    "num_samples_per_proxy": 64,
+    "num_query_per_iter": 100,
+    "inner_lr": 1.0,
+    "outer_lr": 0.1,
+    "num_meta_updates_per_iter": 50, 
+    "entropy_reg_coeff": 0.2,
+    "proxy_oracle": {
+        "model_name": "CNN",
+        "p": 1.0, 
+    },
+    "policy": {
+        "num_steps": 58, # number of steps (per env) before updating... ensure this is at least as big as the length of the episode of the environment
+        "num_meta_steps": 58,
+    },
+    "outerloop": {
+        "oracle": "proxy",
+        "density_penalty": True,
+    },
+    "selection_criteria": { # Configs for selecting the samples
+        "method": "UCB", 
+        "config": {
+            'beta': 1.0,
+        },
+        "diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+    },
+    "env": { # See DynaPPO paper for these configs
+        "lambda": 0.1, # Diversity hyperparameter -- higher is more penalty for more similar mols.
+        "radius": 2, 
+    },
+
+    "true_oracle": {
+        "model_name": "RNA14_Oracle",
+    },
+    "reset_policy_per_round": True,
+    "use_baseline": False,
+    "log_interval": 1,
+    "results_log_dir": "./logs",
+    "seed": 73,
+}
+
+
+
+rna14_025 = { # 023 but beta: 1 -> 2
+    "exp_label": "MetaRLBO-RNA14-025",
+    "task": "RNA14-v0",
+    "num_proxies": 4, 
+    "max_num_queries": 1500, # Maximum number of queries in experiment
+    "num_inner_updates": 2,
+    "num_query_proxies": 32,
+    "num_initial_samples": 100,
+    "num_samples_per_proxy": 64,
+    "num_query_per_iter": 100,
+    "inner_lr": 1.0,
+    "outer_lr": 0.1,
+    "num_meta_updates_per_iter": 50, 
+    "entropy_reg_coeff": 0.2,
+    "proxy_oracle": {
+        "model_name": "CNN",
+        "p": 1.0, 
+    },
+    "policy": {
+        "num_steps": 58, # number of steps (per env) before updating... ensure this is at least as big as the length of the episode of the environment
+        "num_meta_steps": 58,
+    },
+    "outerloop": {
+        "oracle": "proxy",
+        "density_penalty": True,
+    },
+    "selection_criteria": { # Configs for selecting the samples
+        "method": "UCB", 
+        "config": {
+            'beta': 2.0,
+        },
+        "diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+    },
+    "env": { # See DynaPPO paper for these configs
+        "lambda": 0.1, # Diversity hyperparameter -- higher is more penalty for more similar mols.
+        "radius": 2, 
+    },
+
+    "true_oracle": {
+        "model_name": "RNA14_Oracle",
+    },
+    "reset_policy_per_round": True,
+    "use_baseline": False,
+    "log_interval": 1,
+    "results_log_dir": "./logs",
+    "seed": 73,
+}
+
+
+
+rna14_026 = { # 023 but beta: 1 -> 0
+    "exp_label": "MetaRLBO-RNA14-026",
+    "task": "RNA14-v0",
+    "num_proxies": 4, 
+    "max_num_queries": 1500, # Maximum number of queries in experiment
+    "num_inner_updates": 2,
+    "num_query_proxies": 32,
+    "num_initial_samples": 100,
+    "num_samples_per_proxy": 64,
+    "num_query_per_iter": 100,
+    "inner_lr": 1.0,
+    "outer_lr": 0.1,
+    "num_meta_updates_per_iter": 50, 
+    "entropy_reg_coeff": 0.2,
+    "proxy_oracle": {
+        "model_name": "CNN",
+        "p": 1.0, 
+    },
+    "policy": {
+        "num_steps": 58, # number of steps (per env) before updating... ensure this is at least as big as the length of the episode of the environment
+        "num_meta_steps": 58,
+    },
+    "outerloop": {
+        "oracle": "proxy",
+        "density_penalty": True,
+    },
+    "selection_criteria": { # Configs for selecting the samples
+        "method": "UCB", 
+        "config": {
+            'beta': 0.0,
+        },
+        "diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+    },
+    "env": { # See DynaPPO paper for these configs
+        "lambda": 0.1, # Diversity hyperparameter -- higher is more penalty for more similar mols.
+        "radius": 2, 
+    },
+
+    "true_oracle": {
+        "model_name": "RNA14_Oracle",
+    },
+    "reset_policy_per_round": True,
+    "use_baseline": False,
+    "log_interval": 1,
+    "results_log_dir": "./logs",
+    "seed": 73,
+}
+
+
+
+
+rna14_027 = { # 020 but num_meta_updates_per_iter: 50 -> 80
+    "exp_label": "MetaRLBO-RNA14-027",
+    "task": "RNA14-v0",
+    "num_proxies": 4, 
+    "max_num_queries": 1500, # Maximum number of queries in experiment
+    "num_inner_updates": 2,
+    "num_query_proxies": 32,
+    "num_initial_samples": 100,
+    "num_samples_per_proxy": 64,
+    "num_query_per_iter": 100,
+    "inner_lr": 1.0,
+    "outer_lr": 0.1,
+    "num_meta_updates_per_iter": 80, 
+    "entropy_reg_coeff": 0.2,
+    "proxy_oracle": {
+        "model_name": "CNN",
+        "p": 1.0, 
+    },
+    "policy": {
+        "num_steps": 58, # number of steps (per env) before updating... ensure this is at least as big as the length of the episode of the environment
+        "num_meta_steps": 58,
+    },
+    "outerloop": {
+        "oracle": "proxy",
+        "density_penalty": True,
+    },
+    "selection_criteria": { # Configs for selecting the samples
+        "method": "UCB", 
+        "config": {
+            'beta': 1.0,
+        },
+        "diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+    },
+    "env": { # See DynaPPO paper for these configs
+        "lambda": 0.1, # Diversity hyperparameter -- higher is more penalty for more similar mols.
+        "radius": 2, 
+    },
+
+    "true_oracle": {
+        "model_name": "RNA14_Oracle",
+    },
+    "reset_policy_per_round": True,
+    "use_baseline": False,
+    "log_interval": 1,
+    "results_log_dir": "./logs",
+    "seed": 73,
+}

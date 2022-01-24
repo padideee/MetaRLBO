66931221e1d79664de4ca76a7c7ff290829f8afb
Leo Feng <leofeng1999@gmail.com>
2022-01-24 11:03:46 -0500

    new ising20 config

--- config/Ising20_configs.py
+++ config/Ising20_configs.py
@@ -3366,3 +3366,47 @@ metarlbo_ising20_075 = { # Copy 063 but lambda: 1.0 -> 0.5
     "results_log_dir": "./logs",
     "seed": 73,
 }
+
+
+metarlbo_ising20_076 = { # Copy 063  w/ il, ol -> 2.0, 0.2
+    "exp_label": "MetaRLBO-Ising20-CNN-076",
+    "task": "AltIsing20-v0",
+    "num_proxies": 4, 
+    "max_num_queries": 8000, # Maximum number of queries in experiment
+    "num_inner_updates": 1,
+    "num_query_proxies": 32,
+    "num_initial_samples": 500,
+    "num_samples_per_proxy": 64,
+    "num_query_per_iter": 500,
+    "inner_lr": 2.0,
+    "outer_lr": 0.2,
+    "num_meta_updates_per_iter": 80, 
+    "entropy_reg_coeff": 0.2,
+    "proxy_oracle": {
+        "model_name": "CNN",
+        "p": 1.0, 
+    },
+    "outerloop": {
+        "oracle": "proxy",
+        "density_penalty": True,
+    },
+    "selection_criteria": { # Configs for selecting the samples
+        "method": "UCB", 
+        "config": {
+            'beta': 1.0,
+        },
+        "diversity_threshold": 1, # Diversity threshold when greedily selecting molecules...
+    },
+    "env": { # See DynaPPO paper for these configs
+        "lambda": 1.0, # Diversity hyperparameter -- higher is more penalty for more similar mols.
+        "radius": 2, 
+    },
+
+    "true_oracle": {
+        "model_name": "AltIsing_Oracle",
+    },
+    "use_baseline": False,
+    "log_interval": 1,
+    "results_log_dir": "./logs",
+    "seed": 73,
+}
